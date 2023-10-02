0a84adb084bb3819aadf1c232b6939163fada2ea
Neel Nair <neel14nair@gmail.com>
2023-10-02 14:20:54 +0100

    working solution

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,26 @@
 
 def frequencies(items):
     frequencies = {}
+    seen=[]
     # Your code goes here
+    new = ""
+    
+    for i in items:
+        i = str(i)
+        if i != new and i not in seen:
+            new = i
+            seen.append(i)
+    print(seen)
+    
+    
+    for i in seen:
+        count = 0
+        for j in items:
+            j=str(j)
+            if i==j:
+                count +=1
+        frequencies[i] = count
     return frequencies
+
+    
+frequencies(["100",100,100,100,"a","b","a","b","c"])
